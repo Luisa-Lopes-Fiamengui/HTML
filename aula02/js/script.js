f2570f26060e817d1d8598cc01376c7fc0f019b7
Pikanavee <luisamengui07@gmail.com>
2024-03-26 10:16:47 -0300

    atividades

--- aula02/js/script.js
+++ aula02/js/script.js
@@ -1,3 +1,14 @@
-alerta = () =>{
-    alert("Vai toma no seu kool! :333")
-}
+alerta = () => {
+  alert("Vai toma no seu kool! :333");
+};
+const enviar = () => {
+  let nome = document.getElementById(`nome`).value;
+  document.getElementById(
+    "resposta"
+  ).innerHTML = `<h2>ola ${nome}, formulário enviado com sucesso</h2>`;
+  if (nome == "") {
+    alert(`ola, bem vindo`);
+  } else {
+    alert(`ola ${nome}, bem vindo`);
+  }
+};
